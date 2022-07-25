20a212a60bc7c69432679d05791d22c96dc30747
OliverVermeulen <72196313+OliverVermeulen@users.noreply.github.com>
2022-07-25 17:42:27 +0200

    update of massive proportions

--- public/vue/script.js
+++ public/vue/script.js
@@ -1,12 +1,30 @@
-import products from "/Bunker-Online-Store-Fullstack/src/json/products.json" assert { type: "json" };
+import productArray from "/Bunker-Online-Store-Fullstack/src/json/products.json" assert { type: "json" };
 
 const { createApp } = window.Vue;
+
+const filterType = (key, productList) =>
+  productList.filter((item) => item.type === key);
+
 const Component = {
   data() {
     return {
-      products: products,
+      productList: productArray,
+      awesome: true
     };
-  }
+  },
+  computed: {
+    topList() {
+      return filterType("top", this.productList);
+    },
+    bottomList() {
+      return filterType("bottom", this.productList);
+    },
+  },
+  methods: {
+    toggle() {
+      this.awesome = !this.awesome;
+    },
+  },
 };
 
 // mounting app
