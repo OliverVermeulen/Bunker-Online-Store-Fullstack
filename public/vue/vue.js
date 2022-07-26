e46d06905480428c94fadd664bae3cb029148c46
OliverVermeulen <72196313+OliverVermeulen@users.noreply.github.com>
2022-07-26 12:47:43 +0200

    update

--- public/vue/vue.js
+++ public/vue/vue.js
@@ -41,4 +41,19 @@ const Component = {
 window.addEventListener("DOMContentLoaded", () => {
   const app = createApp(Component);
   app.mount("#app");
+
+  // Header functions
+window.onscroll = function () {
+  myFunction();
+};
+let header = document.getElementById("myHeader");
+let sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky"); // Add sticky class
+  } else {
+    header.classList.remove("sticky"); // Remove sticky class
+  }
+};
 });
