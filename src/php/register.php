49232f8ddb44fe80c26e98a1ed2e68532164ee82
OliverVermeulen <72196313+OliverVermeulen@users.noreply.github.com>
2022-07-27 18:07:59 +0200

    save

--- src/php/register.php
+++ src/php/register.php
@@ -4,8 +4,8 @@
 include ("/MAMP/htdocs/Bunker-Online-Store-Fullstack/src/Model/db.php");
 
 // Define variables and initialize with empty values
-$name = $username = $password = $confirm_password = "";
-$name_err = $username_err = $password_err = $confirm_password_err = "";
+$name = $username = $email = $password = $confirm_password = "";
+$name_err = $username_err = $email_err =  $password_err = $confirm_password_err = "";
 
 // Processing form data when form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -51,6 +51,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
     }
 
+    if (empty(trim($_POST["email"]))) {
+        $email_err = "Please enter a email address";
+    } else {
+        $email = trim($_POST["email"]);
+    }
+
     // Validate password
     if (empty(trim($_POST["password"]))) {
         $password_err = "Please enter a password.";
@@ -71,18 +77,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
     // Check input errors before inserting in database
-    if (empty($name_err) && empty($username_err) && empty($password_err) && empty($confirm_password_err)) {
+    if (empty($name_err) && empty($username_err) && empty($email_err) && empty($password_err) && empty($confirm_password_err)) {
 
         // Prepare an insert statement
-        $sql = "INSERT INTO users (name, username, password) VALUES (?, ?, ?)";
+        $sql = "INSERT INTO users (name, username, email, password) VALUES (?, ?, ?, ?)";
 
         if ($stmt = mysqli_prepare($link, $sql)) {
             // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "sss", $param_name, $param_username, $param_password);
+            mysqli_stmt_bind_param($stmt, "ssss", $param_name, $param_username, $param_email, $param_password);
 
             // Set parameters
             $param_name = $name;
             $param_username = $username;
+            $param_email = $email;
             $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
 
             // Attempt to execute the prepared statement
