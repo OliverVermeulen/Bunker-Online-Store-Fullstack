a7207fa035b76ea453921ecd2343f75b36716a59
OliverVermeulen <72196313+OliverVermeulen@users.noreply.github.com>
2022-07-28 11:02:46 +0200

    save

--- src/php/login.php
+++ src/php/login.php
@@ -1,80 +1,80 @@
 <?php
 // Initialize the session
 session_start();
-
+ 
 // Check if the user is already logged in, if yes then redirect him to welcome page
-if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
-    header("location: /Bunker-Online-Store-Fullstack/");
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    header('Location: /Bunker-Online-Store-Fullstack/');
     exit;
 }
 
 // Include config file
 include ("/MAMP/htdocs/Bunker-Online-Store-Fullstack/src/Model/db.php");
-
+ 
 // Define variables and initialize with empty values
-$username = $password = "";
-$username_err = $password_err = $login_err = "";
-
+$email = $password = "";
+$email_err = $password_err = $login_err = "";
+ 
 // Processing form data when form is submitted
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
     // Check if username is empty
-    if (empty(trim($_POST["username"]))) {
-        $username_err = "Please enter username.";
-    } else {
-        $username = trim($_POST["username"]);
+    if(empty(trim($_POST["email"]))){
+        $email_err = "Please enter username.";
+    } else{
+        $email = trim($_POST["email"]);
     }
-
+    
     // Check if password is empty
-    if (empty(trim($_POST["password"]))) {
+    if(empty(trim($_POST["password"]))){
         $password_err = "Please enter your password.";
-    } else {
+    } else{
         $password = trim($_POST["password"]);
     }
-
+    
     // Validate credentials
-    if (empty($username_err) && empty($password_err)) {
+    if(empty($email_err) && empty($password_err)){
         // Prepare a select statement
-        $sql = "SELECT id, username, password FROM users WHERE username = ?";
-
-        if ($stmt = mysqli_prepare($link, $sql)) {
+        $sql = "SELECT id, email, password FROM users WHERE email = ?";
+        
+        if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "s", $param_username);
-
+            mysqli_stmt_bind_param($stmt, "s", $param_email);
+            
             // Set parameters
-            $param_username = $username;
-
+            $param_email = $email;
+            
             // Attempt to execute the prepared statement
-            if (mysqli_stmt_execute($stmt)) {
+            if(mysqli_stmt_execute($stmt)){
                 // Store result
                 mysqli_stmt_store_result($stmt);
-
+                
                 // Check if username exists, if yes then verify password
-                if (mysqli_stmt_num_rows($stmt) == 1) {
+                if(mysqli_stmt_num_rows($stmt) == 1){                    
                     // Bind result variables
-                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
-                    if (mysqli_stmt_fetch($stmt)) {
-                        if (password_verify($password, $hashed_password)) {
+                    mysqli_stmt_bind_result($stmt, $id, $email, $hashed_password);
+                    if(mysqli_stmt_fetch($stmt)){
+                        if(password_verify($password, $hashed_password)){
                             // Password is correct, so start a new session
                             session_start();
-
+                            
                             // Store data in session variables
                             $_SESSION["loggedin"] = true;
                             $_SESSION["id"] = $id;
-                            $_SESSION["username"] = $username;
-
+                            $_SESSION["email"] = $email;                            
+                            
                             // Redirect user to welcome page
-                            header("location: /sql-user-authentication-app/index.php");
-                        } else {
+                            header('Location: /Bunker-Online-Store-Fullstack/');
+                        } else{
                             // Password is not valid, display a generic error message
                             $login_err = "Invalid username or password.";
                         }
                     }
-                } else {
+                } else{
                     // Username doesn't exist, display a generic error message
                     $login_err = "Invalid username or password.";
                 }
-            } else {
+            } else{
                 echo "Oops! Something went wrong. Please try again later.";
             }
 
@@ -82,8 +82,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             mysqli_stmt_close($stmt);
         }
     }
-
+    
     // Close connection
     mysqli_close($link);
 }
+header('Location: /Bunker-Online-Store-Fullstack/');
 ?>
